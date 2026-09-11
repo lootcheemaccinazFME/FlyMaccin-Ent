07bdf5c33dbfe05535927becfc26efbd8aad1071
lootcheemaccinazFME <lootcheemaccinaz@gmail.com>
2026-09-11 03:13:27 -0700

    Upgrade Demonic DAW to v1.1.0 internet instruments

--- demonic-daw-android/app/build.gradle.kts
+++ demonic-daw-android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.flymaccin.demonicdaw"
         minSdk = 26
         targetSdk = 35
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 110
+        versionName = "1.1.0"
     }
 
     buildTypes {
