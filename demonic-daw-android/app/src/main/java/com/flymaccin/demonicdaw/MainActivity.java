79ea14dbd4ceaaf79075239abbc98feb6126273a
lootcheemaccinazFME <lootcheemaccinaz@gmail.com>
2026-09-11 03:16:16 -0700

    Fix Android permission callback visibility

--- demonic-daw-android/app/src/main/java/com/flymaccin/demonicdaw/MainActivity.java
+++ demonic-daw-android/app/src/main/java/com/flymaccin/demonicdaw/MainActivity.java
@@ -292,7 +292,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == STORAGE_PERMISSION_REQUEST) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
