f6fd1ca57c20331105553f57219bfc34ab263b88
lootcheemaccinazFME <lootcheemaccinaz@gmail.com>
2026-09-11 04:07:27 -0700

    Fix Android shortcut imports for v2.1.1 build

--- .github/scripts/patch_demonic_mobile_multitask.py
+++ .github/scripts/patch_demonic_mobile_multitask.py
@@ -12,11 +12,11 @@ main.write_text(r'''package com.flymaccin.bookwriter;
 
 import android.app.Activity;
 import android.app.PictureInPictureParams;
-import android.app.ShortcutInfo;
-import android.app.ShortcutManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
 import android.graphics.drawable.Icon;
 import android.net.Uri;
 import android.os.Build;
