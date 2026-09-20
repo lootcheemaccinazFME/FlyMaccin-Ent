c562580c3e6277e3a347f7687cc5cb444f4eca0e
lootcheemaccinazFME <lootcheemaccinaz@gmail.com>
2026-09-20 02:53:01 -0700

    Add Demonic TV standalone and merge-ready scaffold

---
+++ android/demonic-tv/app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "com.flymaccin.demonictv"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.flymaccin.demonictv"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 100
+        versionName = "1.0.0-alpha1"
+    }
+}
